7e2ffe5976576278cfd0c894ee59182a4ac17635
tdq45gj <61915096+tdq45gj@users.noreply.github.com>
2020-07-06 18:15:40 +0800

    get account info
    added new methods to get account info (first/last name, email, enrollment status)

--- server.py
+++ server.py
@@ -154,6 +154,13 @@ def createUser():
         'jwt': token,
     })
 
+@app.route('/api/account')
+@jwt_required
+def getAccountInfo():
+    userId = getRequesterIdInt()
+    accountInfo = accountHandler.getInfo(userId)
+
+    return jsonify(jsonpickle.decode(jsonpickle.encode(accountInfo)))
 
 createResourceSchema = {
     'required': ['name'],
@@ -244,7 +251,6 @@ def createConnectionRequest():
 def resolveConnectionRequest(connectionRequestId):
     connectionRequests.markResolved(connectionRequestId)
     return jsonMessageWithCode('success')
-  
 
 createEventSchema = {
     'required': ['name'],
