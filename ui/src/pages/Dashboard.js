b0fb2b7b99f8d10db554572e49bbade9d13dbb29
GrahamGoudeau <grahamgoudeau@gmail.com>
2020-07-22 01:52:49 -0400

    Remove un-implemented text from email body

--- ui/src/pages/Dashboard.js
+++ ui/src/pages/Dashboard.js
@@ -108,7 +108,7 @@ function Dashboard(props) {
 }
 
 function buildMailToLink(request) {
-    return `mailto:${request.requestee.email}?cc=${request.requester.email}&?subject=MCG%20Alumni%20Portal%3A%20Networking%20Request&body=Hi%20${Name(request.requestee.name)}%2C%0D%0A%0D%0A${Name(request.requester.name)}%20found%20your%20profile%20on%20the%20MCG%20Alumni%20Portal%2C%20and%20would%20like%20to%20connect.%20They%20are%20cc'd%20here.%20Their%20message%3A%20%22${request.message}%22%0D%0A%0D%0A`
+    return `mailto:${request.requestee.email}?cc=${request.requester.email}&?subject=MCG%20Alumni%20Portal%3A%20Networking%20Request&body=Hi%20${Name(request.requestee.name)}%2C%0D%0A%0D%0A${Name(request.requester.name)}%20found%20your%20profile%20on%20the%20MCG%20Alumni%20Portal%2C%20and%20would%20like%20to%20connect.%20They%20are%20cc'd%20here.`
 }
 
 export default Dashboard;
